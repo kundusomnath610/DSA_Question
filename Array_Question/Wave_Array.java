f660bbd4057c425c3a0a191bd9528335febcf4a9
Somnath Kundu <kundusomnath610@gmail.com>
2024-06-12 00:53:46 +0530

    initial commit

--- Array_Question/Wave_Array.java
+++ Array_Question/Wave_Array.java
@@ -1,3 +1,20 @@
+/*
+ * Question :: 
+ * 
+ *  Given a sorted array arr[] of distinct integers. 
+ *  Sort the array into a wave-like array(In Place).
+    In other words, arrange the elements into a sequence 
+    such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....
+
+    If there are multiple solutions, find the lexicographically smallest one.
+
+    Note:The given array is sorted in ascending order, 
+    and you don't need to return anything to make changes 
+    in the original array itself.
+ */
+
+
+
 public class Wave_Array {
     public static void main(String[] args) {
         int [] array = {1,2,3,4,5};
@@ -16,3 +33,8 @@ public class Wave_Array {
         }
     }
 }
+
+/*
+ * Time Complexity :: O(n)
+ * Space Complexity :: O(n)
+ */
