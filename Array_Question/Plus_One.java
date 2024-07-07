73e4327f5f125516c44e44b4742826eba2904cff
Somnath Kundu <kundusomnath610@gmail.com>
2024-07-07 23:57:33 +0530

    initial commit

--- Array_Question/Plus_One.java
+++ Array_Question/Plus_One.java
@@ -18,3 +18,34 @@ public class Plus_One {
         return newNumber;
     }
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * This is ArrayList Approch.. in GFG ..
+ * 
+ * class Solution {
+    static ArrayList<Integer> increment(ArrayList<Integer> arr , int N) 
+    {
+        // code here
+       for (int i = N - 1; i >= 0; i--) {
+            // If the current element is less than 9, increment it and return the list
+            if (arr.get(i) < 9) {
+                arr.set(i, arr.get(i) + 1);
+                return arr;
+            } else {
+                // Set the current element to 0 if it's 9
+                arr.set(i, 0);
+            }
+        }
+        // If all elements were 9, we need a new list with size N + 1
+        ArrayList<Integer> Arr = new ArrayList<>(N + 1);
+        // Initialize the new list
+        Arr.add(1); // First element should be 1
+        for (int i = 1; i <= N; i++) {
+            Arr.add(0); // All other elements should be 0
+        }
+        return Arr;
+    }
+};
+ */
