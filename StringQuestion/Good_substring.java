b488f191f5bdb9f3488ee0de92a41a759d3fe88d
Somnath Kundu <kundusomnath610@gmail.com>
2024-08-25 22:18:17 +0530

    initial commit

--- StringQuestion/Good_substring.java
+++ StringQuestion/Good_substring.java
@@ -7,19 +7,19 @@ public class Good_substring {
     public static long cntGood(String S) {
         // code here
         int n = S.length();
-        int w = 0;
+        int counter = 0;
         long count = 0;
         
         for(int i=0; i<n; i++) {
             int a = S.charAt(i) - '0';
             
-            if(a %2 != 0) {
-               count += i+1;
-               count -= w; 
+            if(a %2 != 0) { // Cheak the number is odd
+               count += i+1; // if odd the current index plus one
+               count -= counter; // counter is zero then it not move..
             }
             
-            if(a == 0) {
-                w++;
+            if(a == 0) { // if current element is zero then counter move forword.
+                counter++;
             }
         }
         return count;
